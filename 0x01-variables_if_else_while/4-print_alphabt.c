0dc9eb47062bd4130f7c556155d33e2f4cac1631
maryam <cakemariam1234@gmail.com>
2023-09-08 16:59:52 -0700

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,14 @@
 int main(void)
 {
 	char a = 'a';
-	
+
 	while (a < 'z')
 	{
-		if (a != 'q' || a != 'e')
-		{
-			putchar(a);
-		}
-		else
+		if (a == 'q' || a == 'e')
 		{
 			a++;
 		}
+		putchar(a);
 		a++;
 	}
 	putchar('\n');
