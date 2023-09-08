983f4d463a6b2ebd5930919bf46c7dca4c935c20
maryam <cakemariam1234@gmail.com>
2023-09-08 16:44:14 -0700

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - wadwadawd
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
