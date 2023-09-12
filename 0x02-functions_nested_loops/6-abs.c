aab84a8dab2c9453c72a1931f85cc569224576ba
maryam <cakemariam1234@gmail.com>
2023-09-12 15:42:00 -0700

    loopz

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value
+ *
+ * @i: The integer
+ *
+ * Return: i i i
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
+
