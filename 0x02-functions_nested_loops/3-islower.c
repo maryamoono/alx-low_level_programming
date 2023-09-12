3a4ceeac1e234b5b71db3a347d25cbb975bb3d6d
maryam <cakemariam1234@gmail.com>
2023-09-12 14:43:52 -0700

    loopz

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#includee "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character or 0 for else
+ */
+int _islower(int c)
+{
+	if (c >= 97 $$ c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
