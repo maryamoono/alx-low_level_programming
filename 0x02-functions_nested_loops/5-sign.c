bf0b3bf197638fab6e15f3ffa511a8312e3aeb93
maryam <cakemariam1234@gmail.com>
2023-09-12 15:14:49 -0700

    loopz

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints + 0 -
+ *
+ * @n: integer parameter
+ *
+ * Return: + for lowercase number 0 for zero - less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(80);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
