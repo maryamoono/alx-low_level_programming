be625342d9faba908821aece72dee41778e0ec83
maryam <cakemariam1234@gmail.com>
2023-09-14 13:50:10 -0700

    now

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - cause an infinity loopz
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	
+	/* while (i < 10) */
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
