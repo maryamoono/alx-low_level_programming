44fa969ecb81046b3f1d6b162464cec19fe2b77d
maryam <cakemariam1234@gmail.com>
2023-09-18 14:49:54 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - the code must check
+ *
+ * @a: int a
+ * @b: int b
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	temp = *b;
+	*a = *b;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - the code must cheak
+ *
+ * @s: intr
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
