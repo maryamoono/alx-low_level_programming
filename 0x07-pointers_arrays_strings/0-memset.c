20c456232dfd1d545376a1a57930698e01841d7b
maryam <cakemariam1234@gmail.com>
2023-09-25 11:39:14 -0700

    i memes is funny

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+/*
+ * _memset - hellllllllllllllllllllllllllllllllllllllllo
+ * @s: maryam taheeer
+ * @b: ossmamamma taheere
+ * @n: who are yo
+ * Return: always nothing it means s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int o;
+
+	o = 0;
+	while (o < n)
+	{
+		o++;
+		s[o] = b;
+	}
+	return (s);
+}
