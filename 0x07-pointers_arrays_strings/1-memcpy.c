57421ef957bc23c8cd3830c508538bf02fae1cda
maryam <cakemariam1234@gmail.com>
2023-09-25 13:17:47 -0700

    jhfeshfhejfhj

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,24 @@
-:
+#include "main.h"
+#include "stdio.h"
+/**
+ * _memcpy - copy funy memes hahahahahahhahahah
+ * @dest: btanqwswwwd
+ * @src: sources of stupid pesron that i was like
+ * @n: maryam want to sleep
+ * Return: what i will told you then
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int w;
+	char *q = dest;
+
+	w = 0;
+	while (w < n)
+	{
+		w++;
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (q);
+}
