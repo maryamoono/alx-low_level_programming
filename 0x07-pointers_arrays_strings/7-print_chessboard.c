f9a7fb21a057bc0d60e82fcc950fc5e6cefedc77
maryam <cakemariam1234@gmail.com>
2023-09-25 15:57:29 -0700

    like yoU

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_chessboard - lab was achemist one day
- * @a: llllllllllllllllllllllllllllllllllllllllldwadwad;l;dl
+ * @a: is acharacter that want check
  * return: nxksx
  */
 void print_chessboard(char (*a)[8])
