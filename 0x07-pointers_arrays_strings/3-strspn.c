32f95953e63b22b953a692e1df2113caf42e492c
maryam <cakemariam1234@gmail.com>
2023-09-25 17:07:32 -0700

    lkjhgfdsa

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,33 +3,31 @@
 #include "string.h"
 /**
  * _strspn - i dont know njjbhbbb mbbnm
- *
  * @s: tmama
  * @accept: tmama
- *
  * Return: aletter
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int m, s;
-	unsigned int count = 0;
+	unsigned int m, o;
+	unsigned int spn = 0;
 	unsigned int found = 0;
 
-	for (m = 0 ; m < strlen(s) ; m++);
+	for (m = 0 ; m < strlen(s) ; m++)
 	{
-		for (s = 0 ; s < strlen(accept) ; s++)
+		for (o = 0 ; o < strlen(accept) ; o++)
 		{
-			if (s[m] == accept[t])
+			if (s[m] == accept[o])
 			{
 				found = 1;
 				break;
 			}
 		}
-	if (found)
-		numk++;
-	else
-		break;
-	found = 0;
+		if (found)
+			spn++;
+		else
+			break;
+		found = 0;
 	}
-	return (numk);
+	return (spn);
 }
