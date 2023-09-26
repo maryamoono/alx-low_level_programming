81a3a01ed6e60866b7dd3e51d9ccedeae8e37039
maryam <cakemariam1234@gmail.com>
2023-09-26 13:18:49 -0700

    oiuytrew

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,31 +2,30 @@
 #include "string.h"
 /**
  * cap_string - capitalizion words
- * @s: from string that input
+ * @r: from string that input
  * Return: alwayes dest
  */
 
-char *cap_string(char *s)
+char *cap_string(char *r)
 {
-	char *cap_string(char *s)
-{
-	int count = 0, y;
+	int lem = 0, e;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	if (*(r + lem) >= 97 && *(r + lem) <= 122)
+		*(r + lem) = *(r + lem) - 32;
+	lem++;
+	while (*(r + lem) != '\0')
 	{
-		for (y = 0; y < 13; y++)
+		for (e = 0; e < 13; e++)
 		{
-			if (*(s + count) == sep_words[i])
+			if (*(r + lem) == sep_words[e])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				if ((*(r + (lem + 1)) >= 97) && (*(r + (lem + 1)) <= 122))
+					*(r + (lem + 1)) = *(r + (lem + 1)) - 32;
 				break;
 			}
 		}
-		count++;
+		lem++;
 	}
-	return (s);
+	return (r);
+}
