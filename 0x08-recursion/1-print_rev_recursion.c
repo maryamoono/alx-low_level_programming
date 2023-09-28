d5918546263e208ba0bf22876c99841ee73a0aac
maryam <cakemariam1234@gmail.com>
2023-09-28 15:20:58 -0700

    loik

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - it want word in back side
+ * @s: help us to print the word
+ * return: omg
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
