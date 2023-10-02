bfdaf393a821e8a17e6ddac0e1f060625228ad7d
maryam <cakemariam1234@gmail.com>
2023-10-02 13:16:44 -0700

    jjd

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - nndandkn
+ * @argc: kiujnhby
+ *
+ * @argv: mlawj
+ * Return: nuukk
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	
+	x = 0;
+	while (x < argc)
+	{
+		x++;
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
