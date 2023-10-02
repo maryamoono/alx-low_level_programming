ba007c97edb0a8555308d99b7f71a4c02fe30855
maryam <cakemariam1234@gmail.com>
2023-10-02 15:16:32 -0700

    hy

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - jdksjdksjdks
+ * @argc: xsbjhjsxs
+ * @atoi: jwkdjkdj
+ * @argv: bgtrfv
+ * Return: kjskqjsk
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 8)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[6]) * atoi(argv[7]));
+		return (0);
+	}
+}
