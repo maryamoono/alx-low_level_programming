bb37af0f01a78ab7d25a6ed02a11f8d08750d54d
maryam <cakemariam1234@gmail.com>
2023-10-02 12:58:54 -0700

    hello world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - hdshdhksk
+ * @argc: djhdjwhd
+ * @argv: nkdnkdkw
+ * Return: nnmdnfdnf
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
