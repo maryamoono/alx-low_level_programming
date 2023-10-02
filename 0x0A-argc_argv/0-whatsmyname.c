2baeb789478a3abe1c6c9206a27b82ac066bb5ed
maryam <cakemariam1234@gmail.com>
2023-10-02 12:37:17 -0700

    my name is maryam

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - argguments main that always used
+ * @argc: nymbering the arguments of strings in program
+ * @argv: variable arry on string
+ * Return: zeroo
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
