9b2288ec60e404f665b84469e0bae45ca7a79d4f
maryam <cakemariam1234@gmail.com>
2023-10-04 16:35:08 -0700

    lol

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,31 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array - creat wgat i can 
+ * create_array - creat wgat i can
  * @c: craxk in the line
  * @size: i dont wnhd
  * Return: Nothing.
  */
 char *create_array(unsigned int size, char c)
 {
-    unsigned int m;
-    char *y;
+	char *co;
+	unsigned int s;
 
-    if (size == 0)
-    {
-	    return (NULL);
-    }
+	if (size == 0)
+		return (NULL);
 
-    y = malloc(sizeof(c) * size);
+	co = malloc(sizeof(c) * size);
 
-    if (y == 0)
-	 return (NULL);
-    m = 0;
-    while (m < size)
-    {
-	    m++;
-            y[m] = c;
-    }
-    return (y);
+	if (co == NULL)
+		return (NULL);
+
+	for (s = 0 ; s < size ; s++)
+	{
+		co[s] = c;
+	}
+	return (co);
 }
-
