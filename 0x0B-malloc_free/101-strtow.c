0e5a75fc3b706fc0037330db5aa23f342c449642
maryam <cakemariam1234@gmail.com>
2023-10-05 15:45:59 -0700

    pm

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,64 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * argstostr - dllmwk km njc
- * @av: dbcg odj kil
- * @ac: klwdlk
- * Return: ol on chdb qwas
+ * ch_free_grid - dkc aq
+ * @grid: dwdk
+ * @height: qwrf
+ * Return: nn k
  */
-char *argstostr(int ac, char **av)
+void ch_free_grid(char **grid, unsigned int height)
 {
-	char *fam;
-	int m, o, y, sh;
-
-	if (ac == 0)
-		return (NULL);
-
-	for (m = o = 0; o < ac; o++)
+	if (grid != 0 && height != NULL)
 	{
-		if (av[o] == NULL)
-			return (NULL);
-
-		for (y = 0; av[o][y] != '\0'; y++)
-			m++;
-		m++;
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
 	}
+}
+/**
+ * strtow - flfklf
+ * @str: jhink h
+ * Return: kkv o
+ */
+char **strtow(char *str)
+{
+	char **fam;
+	unsigned int m, joy, o, y, sh;
 
-	fam = malloc((m + 1) * sizeof(char));
-
-	if (fam == NULL)
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (m = joy = 0; str[m] != '\0'; m++)
+		if (str[m] != ' ' && (str[m + 1] == ' ' || str[m + 1] == '\0'))
+			joy++;
+	aout = malloc((joy + 1) * sizeof(char *));
+	if (fam == NULL || joy == 0)
 	{
 		free(fam);
 		return (NULL);
 	}
-
-	for (o = y = ia = 0; sh < m; y++, sh++)
+	for (o = sh = 0; o < joy; o++)
 	{
-		if (av[o][y] == '\0')
+		for (m = sh; str[m] != '\0'; m++)
 		{
-			fam[sh] = '\n';
-			o++;
-			sh++;
-			y = 0;
+			if (str[m] == ' ')
+				sh++;
+			if (str[m] != ' ' && (str[m + 1] == ' ' || str[m + 1] == '\0'))
+			{
+				fam[o] = malloc((m - sh + 2) * sizeof(char));
+				if (fam[o] == NULL)
+				{
+					ch_free_grid(fam, o);
+					return (NULL);
+				}
+				break;
+			}
 		}
-		if (sh < m - 1)
-			fam[sh] = av[o][y];
+		for (y = 0; sh <= m; sh++, y++)
+			fam[o][y] = str[sh];
+		aout[o][y] = '\0';
 	}
-	fam[sh] = '\0';
-
+	fam[o] = NULL;
 	return (fam);
 }
