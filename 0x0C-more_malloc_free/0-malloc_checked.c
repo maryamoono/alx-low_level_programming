934c29faaf85a7b725dfb26df914635b4626aec8
maryam <cakemariam1234@gmail.com>
2023-10-08 14:59:15 -0700

    olo

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - kjlwjdl nhn 
+ * @b: the swlk
+ * Return: mk
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *l = malloc(b);
+
+	if (l == 0)
+		exit(98);
+	return (l);
+}
