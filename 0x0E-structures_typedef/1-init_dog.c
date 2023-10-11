c4d94da1f1689d1e24af4d1d3315bb2ce16765d8
maryam <cakemariam1234@gmail.com>
2023-10-10 17:07:26 -0700

    ojoi

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog -euiryeiruiu
+ * @d: irueouroei
+ * @name: ,nhnkhm
+ * @age: flpelfpr
+ * @owner: fefjli
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+	}
+}
