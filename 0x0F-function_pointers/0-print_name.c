a8d1fdd64da079d1338505e574b285da1822c4ec
maryam <cakemariam1234@gmail.com>
2023-10-11 16:25:07 -0700

    okl

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - kjdkhhsekk
+ * @name: djksdsj
+ * @f: smxlksms
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
