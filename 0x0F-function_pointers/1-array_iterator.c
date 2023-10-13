b92e2d4798b866cf8d8e4e9b8436e65df8d6c189
Maryam <cakemariam1234@gmail.com>
2023-10-13 11:20:30 -0700

    lkm

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+include "function_pointers.h"
+/**
+ * array_iterator - ljele mlmlv
+ * @array: dkldkld kjdl
+ * @size: dwdmlklkf  mdkd
+ * @action: kjwldjldkl
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int s;
+
+	if (array && action)
+		for (s = 0; s < size; s++)
+			action(array[s]);
+}
