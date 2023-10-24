7bd5c88f3052a90462c6f7cf9ec1fbae9ce5a88a
Maryam <cakemariam1234@gmail.com>
2023-10-24 16:08:18 -0700

    lol

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * strchr - numnumumun the maitrix moive4
+ * _strchr - numnumumun the maitrix moive4
  * @c: catch me if you can
  * @s: idd jksjdksjkdjsk
  * Return: let me think
  */
 char *_strchr(char *s, char c)
 {
-       	int u;
+	int u;
 
 	for (u = 0 ; s[u] != '\0'; u++)
 	{
