9f1d973171bdb90676d93b0cd87dc5cc89e5c8e9
Maryam <cakemariam1234@gmail.com>
2023-10-26 14:42:40 -0700

    lol

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - njbhvg
+ * @b: njbh
+ * Return: njbhvg
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int s, a, num = 0;
+
+	if (b == NULL)
+		return (0);
+	for (s = (strlen(b) - 1) ; s >= 0 ; s--)
+	{
+		if (b[s] == '1' || b[s] == '0')
+		{
+			num += ((b[s] - '0') << a);
+			a++;
+		}
+		else
+			return (0);
+	}
+	return (num);
+}
