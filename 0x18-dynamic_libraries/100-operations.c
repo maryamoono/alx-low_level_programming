4e1c01336504b529bf42f4fbe74439f1c1f89805
Maryam <cakemariam1234@gmail.com>
2023-12-11 17:20:22 -0800

    lol

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - thikm
+ * @a: kin
+ * @b: lokm
+ * Return: kol
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - kil
+ * @a: abnd
+ * @b: mary
+ * Return: osama
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - vim
+ * @a: uo
+ * @b: jnb
+ * Return: hbgv
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - hnbg
+ * @a: vom
+ * @b: lol
+ * Return: kml
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - lo
+ * @a: jnhb
+ * @b: kjnhbgv
+ * Return: bhvgcfdxzs
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
